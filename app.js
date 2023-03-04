aa0599e1b667e4161d4f450264ef02ea2af1ad52
Nelbi Leon <nelbi.leon@gmail.com>
2023-03-04 17:18:32 +0100

    changed images for blue frame and added description

--- app.js
+++ app.js
@@ -1,124 +1,158 @@
 const bonus = [
 {
-  name: 'arbre_nelbi',
-  img: 'images/Arbre_nelbi.png',
-  bigImg: 'big_images/14.png'
+  name: 'BONUS, vous avez trouvé toute les pairs',
+  img: 'blue_images/Arbre_nelbi.png',
+  bigImg: 'big_images/14.png',
+  description: 'D\'où vient le prénom Nelbi ? C\'est à l\'origine le nom d\'un arbre sacré pour une tribu (les Peuls) au Mali. Il y a trois sortes de Nelbi : le Nelbi d\'eau, le Nelbi de terre et le Nelbi de nulle part 🤓'
 }
 ]
 const cardArray = [
 
   {
-    name: 'Esprit_d_equipe',
-    img: 'images/Esprit_d_equipe.png'
-
+    name: 'Esprit d\'equipe',
+    img: 'blue_images/Esprit_d_equipe.png',
+    bigImg: 'big_images/1.png',
+    description: '\'Il y a plus d\'idées dans deux têtes que dans une\'\n\n~ Jaques Chirac'
   },
   {
     name: 'Perseverance',
-    img: 'images/Perseverance.png'
+    img: 'blue_images/Perseverance.png',
+    bigImg: 'big_images/5.png',
+    description: 'En persévérant on arrive à tout.'
   },
   {
-    name: 'Plongee',
-    img: 'images/Plongee.png',
-    bigImg: 'images/big_plongée.jpg'
+    name: 'Plongée',
+    img: 'blue_images/Plongee.png',
+    bigImg: 'images/big_plongée.jpg',
+    description: '📸 prise à Hawaii lors d\'un séjour plongée.\n\n Titulaire du PADI Open Water, pratique la plongée depuis plusieurs années. 🐳'
   },
   {
-    name: 'Porte_bijoux',
-    img: 'images/Porte_bijoux.png',
-    bigImg: 'big_images/15.png'
+    name: 'Porte bijoux',
+    img: 'blue_images/Porte_bijoux.png',
+    bigImg: 'big_images/15.png',
+    description: 'Porte-bijou en laiton conçu dans la tête, et fabriqué, à la main 🤌'
   },
   {
-    name: 'Resolution_de_probleme',
-    img: 'images/Resolution_de_probleme.png'
+    name: 'Resolution de probleme',
+    img: 'blue_images/Resolution_de_probleme.png',
+    bigImg: 'big_images/2.png',
+    description: 'Recherche de solutions innovantes et efficace pour la résolution de problème.'
   },
   {
-    name: 'Sens_de_loptimisation',
-    img: 'images/Sens_de_loptimisation.png'
+    name: 'Sens de loptimisation',
+    img: 'blue_images/Sens_de_loptimisation.png',
+    bigImg: 'big_images/3.png',
+    description: 'Intérêt marqué dans la recherche de moyens pour rendre les processus plus efficaces et performants.'
   },
   {
-    name: 'Squelette_de_baleine',
-    img: 'images/Squelette_baleine.png',
-    bigImg: 'big_images/10.png'
+    name: 'Squelette de baleine',
+    img: 'blue_images/Squelette_baleine.png',
+    bigImg: 'big_images/10.png',
+    description: '📸 prise lors du soclage d\'un squelette de baleine (actuellement exposé au Musée des Confluences, à Lyon)'
   },
   {
     name: 'Velo',
-    img: 'images/Velo.png',
-    bigImg: 'big_images/19.png'
+    img: 'blue_images/Velo.png',
+    bigImg: 'big_images/19.png',
+    description: 'Le vélo, c\'est tout les jours, qu\'il vente ou qu\'il pleuve, court trajet, long trajet .\n🚴🏼‍♀️'
   },
   {
     name: 'Yoga',
-    img: 'images/Yoga.png',
-    bigImg: 'big_images/8.png'
+    img: 'blue_images/Yoga.png',
+    bigImg: 'big_images/8.png',
+    description: 'Titulaire d\'un Teacher Training de 75H et pratique le yoga plusieurs fois par semaine'
   },
   {
     name: 'Humour',
-    img: 'images/Humour.png'
+    img: 'blue_images/Humour.png',
+    bigImg: 'big_images/6.png',
+    description: 'ERREUR 404'
   },
   {
-    name: 'Esprit_d_equipe',
-    img: 'images/Esprit_d_equipe.png'
-
+    name: 'Esprit d\'equipe',
+    img: 'blue_images/Esprit_d_equipe.png',
+    bigImg: 'big_images/1.png',
+    description: '\'Il y a plus d\'idées dans deux têtes que dans une\'\n\n~ Jaques Chirac'
   },
   {
     name: 'Perseverance',
-    img: 'images/Perseverance.png'
+    img: 'blue_images/Perseverance.png',
+    bigImg: 'big_images/5.png',
+    description: 'En persévérant on arrive à tout.'
   },
   {
     name: 'Plongee',
-    img: 'images/Plongee.png',
-    bigImg: 'images/big_plongée.jpg'
+    img: 'blue_images/Plongee.png',
+    bigImg: 'images/big_plongée.jpg',
+    description: '📸 prise à Hawaii lors d\'un séjour plongée.\n\n Titulaire du PADI Open Water, pratique la plongée depuis plusieurs années. 🐳'
   },
   {
-    name: 'Porte_bijoux',
-    img: 'images/Porte_bijoux.png',
-    bigImg: 'big_images/15.png'
+    name: 'Porte bijoux',
+    img: 'blue_images/Porte_bijoux.png',
+    bigImg: 'big_images/15.png',
+    description: 'Porte-bijou en laiton conçu dans la tête, et fabriqué, à la main 🤌'
   },
   {
-    name: 'Resolution_de_probleme',
-    img: 'images/Resolution_de_probleme.png'
+    name: 'Resolution de probleme',
+    img: 'blue_images/Resolution_de_probleme.png',
+    bigImg: 'big_images/2.png',
+    description: 'Recherche de solutions innovantes et efficace pour la résolution de problème.'
   },
   {
-    name: 'Sens_de_loptimisation',
-    img: 'images/Sens_de_loptimisation.png'
+    name: 'Sens de loptimisation',
+    img: 'blue_images/Sens_de_loptimisation.png',
+    bigImg: 'big_images/3.png',
+    description: 'Intérêt marqué dans la recherche de moyens pour rendre les processus plus efficaces et performants.'
   },
   {
-    name: 'Squelette_de_baleine',
-    img: 'images/Squelette_baleine.png',
-    bigImg: 'big_images/10.png'
+    name: 'Squelette de baleine',
+    img: 'blue_images/Squelette_baleine.png',
+    bigImg: 'big_images/10.png',
+    description: '📸 prise lors du soclage d\'un squelette de baleine (actuellement exposé au Musée des Confluences, à Lyon)'
   },
   {
     name: 'Velo',
-    img: 'images/Velo.png',
-    bigImg: 'big_images/19.png'
+    img: 'blue_images/Velo.png',
+    bigImg: 'big_images/19.png',
+    description: 'Le vélo, c\'est tout les jours, qu\'il vente ou qu\'il pleuve, court trajet, long trajet .\n🚴🏼‍♀️'
   },
   {
     name: 'Yoga',
-    img: 'images/Yoga.png',
-    bigImg: 'big_images/8.png'
+    img: 'blue_images/Yoga.png',
+    bigImg: 'big_images/8.png',
+    description: 'Titulaire d\'un Teacher Training de 75H et pratique le yoga plusieurs fois par semaine'
   },
   {
     name: 'Humour',
-    img: 'images/Humour.png'
+    img: 'blue_images/Humour.png',
+    bigImg: 'big_images/6.png',
+    description: 'ERREUR 404'
   }
   // {
   //   name: 'LAD',
   //   img: 'images/LAD.png'
+  //   description: '📸 du musée du Louvre Abu Dhabi prise lors d\'un chantier de soclage pour l\'exposition \'Dragons et phéonix\''
   // },
   // {
   //   name: 'Montagne',
-  //   img: 'images/Montagne.png'
+  //   img: 'images/Montagne.png',
+  //   description: '📸 prise en randonnée dans les Pyrénées Oriantales.\n\n La montagne, ça vous gagne 🤓'
   // },
   // {
   //   name: 'Soudure',
-  //   img: 'images/Soudure.png'
+  //   img: 'images/Soudure.png',
+  //   description: 'Soudure de précision au PUK pour la fabrication de petit soclage'
   // },
 
   // {
   //   name: 'Vallee_des_temples',
-  //   img: 'images/Vallee_des_temples.png'
+  //   img: 'images/Vallee_des_temples.png',
+  //   description: 'On aime l'aventure et les voyages !\n\n📸prise à la vallée des temples en Sicile'
   // },
   // {
   //   name: 'Aïnu',
-  //   img: 'images/ainu.png'
+  //   img: 'images/ainu.png',
+  //   description: 'Article paru dans le magazine\'The Good Life magazine\', photo prise au Musée Carnavalet lors de l\’installation de la collection permanente.'
   // },
   // {
   //   name: 'AutoCAD',
@@ -127,14 +161,19 @@ const cardArray = [
   // {
   //   name: 'Banjo',
   //   img: 'images/Banjo.png'
+  //   description: 'Adore la musique (🎶électronique, rock, jazz,...🎶)'
   // },
   // {
-  //   name: 'dessin_bureau',
-  //   img: 'images/dessin_bureau.png'
+  //   name: 'Dessin bureau Art Déco',
+  //   img: 'images/dessin_bureau.png',
+  //   bigImg: 'big_images/7.png'
+  //   description: 'Apprentissage, pour les diplômes d\'ébenisterie, du dessin, peinture et perspective ainsi que l\'histoire de l\'art et des styles.\n\nDessin au feutre et aquarelle d\'un \'Bureau de dames\'\n style Art Déco.'
   // },
   // {
-  //   name: 'Empathie',
-  //   img: 'images/Empathie.png'
+  //   name: 'polyglotte',
+  //   img: 'blue_images/polyglotte.png',
+  //   bigImg: 'big_images/polyglotte.png',
+  //   description: '\'Un esprit enfermé dans la langue est en prison.\'\n\n~ Simone Weil'
   // },
 
   // {
@@ -160,7 +199,7 @@ cardArray.sort(() => 0.5 - Math.random())
 
 const modal = document.querySelector('#modal');
 const modalImg = document.querySelector('#modal-img');
-const description = document.querySelector('#description');
+const modalDescription = document.querySelector('#description');
 const closeBtn = document.querySelector('#close-btn');
 const gridDisplay = document.querySelector('#grid')
 let cardsChosen = []
@@ -199,6 +238,7 @@ function checkForMatch() {
   }
   if (cardsChosen[0] == cardsChosen[1]) {
     modalImg.src = cardArray[optionOneId].bigImg;
+    modalDescription.src = cardArray[optionOneId].description;
     modal.classList.add('open');
 
     cards[optionOneId].setAttribute('src', 'images/white.png')
@@ -229,6 +269,85 @@ function flipCard () {
   console.log(cardsChosenIds)
   this.setAttribute('src', cardArray[cardId].img)
   if (cardsChosen.length === 2) {
-    setTimeout(checkForMatch, 1500)
+    setTimeout(checkForMatch, 1000)
   }
 }
+
+//Other code
+
+// cardArray.sort(() => 0.5 - Math.random());
+
+// const modal = document.querySelector('#modal');
+// const modalImg = document.querySelector('#modal-img');
+// const description = document.querySelector('#description');
+// const closeBtn = document.querySelector('#close-btn');
+// const gridDisplay = document.querySelector('#grid');
+// let cardsChosen = [];
+// let cardsChosenIds = [];
+// const cardsWon = [];
+
+// closeBtn.addEventListener('click', function() {
+//   modal.classList.remove('open');
+// });
+
+// function createBoard() {
+//   for (let i = 0; i < cardArray.length; i++) {
+//     const card = document.createElement('img');
+//     card.setAttribute('src', 'images/Back.png');
+//     card.setAttribute('data-id', i);
+//     gridDisplay.appendChild(card);
+//     card.addEventListener('click', flipCard);
+//   }
+// }
+
+// createBoard();
+
+// function foundPair() {
+//   const cardId = this.getAttribute('data-id');
+//   alert(cardArray[cardId].bigImg);
+// }
+
+// function checkForMatch() {
+//   const cards = document.querySelectorAll('img');
+//   const optionOneId = cardsChosenIds[0];
+//   const optionTwoId = cardsChosenIds[1];
+
+//   console.log('checkForMatch !');
+//   if (optionOneId == optionTwoId) {
+//     alert('Vous avez cliqué sur la même carte !');
+//   }
+//   if (cardsChosen[0] == cardsChosen[1]) {
+//     modalImg.src = cardArray[optionOneId].bigImg;
+//     modal.classList.add('open');
+
+//     cards[optionOneId].setAttribute('src', 'images/white.png');
+//     cards[optionTwoId].setAttribute('src', 'images/white.png');
+//     cards[optionOneId].removeEventListener('click', flipCard);
+//     cards[optionTwoId].removeEventListener('click', flipCard);
+//     cardsWon.push(cardsChosen);
+//   }
+//   else {
+//     cards[optionOneId].setAttribute('src', 'images/back.png');
+//     cards[optionTwoId].setAttribute('src', 'images/back.png');
+//   }
+//   cardsChosen = [];
+//   cardsChosenIds = [];
+
+//   if (cardsWon.length == cardArray.length / 2) {
+//     alert('Bravo ! Vous avez gagné !');
+//     modalImg.src = bonus[0].bigImg;
+//     modal.classList.add('open');
+//   }
+// }
+
+// function flipCard () {
+//   const cardId = this.getAttribute('data-id');
+//   cardsChosen.push(cardArray[cardId].name);
+//   cardsChosenIds.push(cardId);
+//   console.log(cardsChosen);
+//   console.log(cardsChosenIds);
+//   this.setAttribute('src', cardArray[cardId].img);
+//   if (cardsChosen.length === 2) {
+//     setTimeout(checkForMatch, 1000);
+//   }
+// }
